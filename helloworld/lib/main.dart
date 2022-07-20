a3e731a0bcbd15ad308a6d747c4f315400ab7072
qqaazz0222 <qqaazz0222@naver.com>
2022-07-20 15:21:42 +0900

    Flutter 공부
    2일차

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -10,25 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('앱임'),
-        ),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
-          ),
-        ),
-      )
+      title: 'HelloWorld',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
     );
   }
 }
